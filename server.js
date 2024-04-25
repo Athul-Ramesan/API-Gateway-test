2899c0bc104f5dcc906c63da7a026c4bd343be8d
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-04-25 15:05:08 +0530

    second commit

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const {setupLogging} = require("./logging");
+const { setupProxies } = require('./proxy');
+const { ROUTES } = require('./routes');
+const { setupAuth } = require('./auth');
+const { setupRateLimit } = require('./ratelimit');
 
 const app = express()
 const port = 3010;
+setupLogging(app)
+setupAuth(app,ROUTES)
+setupProxies(app,ROUTES)
+setupRateLimit(app,ROUTES)
 
-
-app.get('/hello', (req, resp) => {
-    return resp.send('HELLO WORLD!');
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
